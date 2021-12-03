4500aa3fce5272d7e2ce07cfdb051297162963d3
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2021-12-03 16:05:28 +0300

    page status graphs

--- src/components/StatusComponents/ServerStatusList.js
+++ src/components/StatusComponents/ServerStatusList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Row, Col, Card, Collapse, Button, Modal, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
@@ -12,13 +12,17 @@ import Moment from 'react-moment';
 
 const PagesList = ({item, pagesData}) => {
     const [open, setOpen] = useState(false);
+    const [modal, setModal] = useState(false)
+    const [chartData, setChartData] = useState()
+    const [isActiveChart, setIsActiveChart] = useState('1m')
+    const currentPage = useRef({service: '', page: ''})
 
     const toggle = () => {
         setOpen((prevState) => !prevState);
     };
 
-    const showChart = (service_name,page_name) =>{
-        const url = `${process.env.REACT_APP_SERVER_URL}/api/v1/chart/service?service_name=${service_name}&page_name=${page_name}`;
+    const showChart = async (service_name, page_name, time_period, time_value) =>{
+        const url = `${process.env.REACT_APP_SERVER_URL}/api/v1/chart/service?service_name=${service_name}&page_name=${page_name}&time_period=${time_period}&time_value=${time_value}`;
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
         headers.append('Accept', 'application/json');
@@ -29,15 +33,115 @@ const PagesList = ({item, pagesData}) => {
         }
         fetch(url, request)
             .then(async (response) => {
-                if(response.status===200){
-                    console.log(response.data)
-                }
+                const data = await response.json();
+                let chartData = [];
+                Object.entries(data.result.chart).map(([key, value]) => {
+                    let timestamp = key+1;
+                    if (time_period === 'h') timestamp = timestamp*100*60
+                    if (time_period === 'd') timestamp = timestamp*100*60*60
+                    if (time_period === 'm') timestamp = timestamp*100*60*60*24
+                    if (time_period === 'y') timestamp = timestamp*100*60*60*24*30
+                    let time = new Date(new Date().getTime() - timestamp);
+                    chartData.push(formatChartData(time, value))
+                })
+                currentPage.current={service: service_name, page: page_name}
+                setChartData(chartData)
+                setIsActiveChart(`${time_value}${time_period}`)
+                setModal(true)
+            })
+            .catch(error => console.log('page chart error', error))   
+    }
+
+    function updateChart(time_period, time_value){
+        const url = `${process.env.REACT_APP_SERVER_URL}/api/v1/chart/service?service_name=${currentPage.current.service}&page_name=${currentPage.current.page}&time_period=${time_period}&time_value=${time_value}`;
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+        const request = {
+            method: "GET",
+            headers: headers,
+            credentials: "include",
+        }
+        fetch(url, request)
+            .then(async (response) => {
+                const data = await response.json();
+                let chartData = [];
+                Object.entries(data.result.chart).map(([key, value]) => {
+                    let timestamp = key+1;
+                    if (time_period === 'h') timestamp = timestamp*100*60
+                    if (time_period === 'd') timestamp = timestamp*100*60*60
+                    if (time_period === 'm') timestamp = timestamp*100*60*60*24
+                    if (time_period === 'y') timestamp = timestamp*100*60*60*24*30
+                    let time = new Date(new Date().getTime() - timestamp);
+                    chartData.push(formatChartData(time, value))
+                })
+                setChartData(chartData)
+                setIsActiveChart(`${time_value}${time_period}`)
             })
             .catch(error => console.log('page chart error', error))   
     }
 
     return (
         <>
+        <Modal show={modal} onHide={() => setModal(false)}>
+            <Modal.Header closeButton >
+                <h3>{currentPage.current.service}/{currentPage.current.page} </h3>
+            </Modal.Header>
+            <Modal.Body>
+            <ul className="nav d-lg-flex mt-3">
+                    <li className="nav-item">
+                        <button className={`nav-link ${isActiveChart == '1h' ? "active" : "text-muted"}`} onClick={() => updateChart('h', 1) }>
+                            1h
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className={`nav-link ${isActiveChart == '1d' ? "active" : "text-muted"}`} onClick={() => updateChart('d', 1) }>
+                            1d
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className={`nav-link ${isActiveChart == '7d' ? "active" : "text-muted"}`} onClick={() => updateChart('d', 7) } >
+                            7d
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className={`nav-link ${isActiveChart == '1m' ? "active" : "text-muted"}`} onClick={() => updateChart('m', 1) } >
+                            1m
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className={`nav-link ${isActiveChart == '3m' ? "active" : "text-muted"}`} onClick={() => updateChart('m', 3) } >
+                            3m
+                        </button>
+                    </li>
+                    <li className="nav-item">
+                        <button className={`nav-link ${isActiveChart == '6m' ? "active" : "text-muted"}`} onClick={() => updateChart('m', 6) } >
+                            6m
+                        </button>
+                    </li>
+                </ul>
+                <div className="chart-container">
+                    <ResponsiveContainer height={280}>
+                        <LineChart
+                            data={chartData}
+                            margin={{
+                                top: 50,
+                                right: 20,
+                                left: -20,
+                                bottom: 5,
+                            }}
+                        >
+                            {/* <CartesianGrid /> */}
+                            <XAxis stroke="#adb5bd" dataKey="x" fontSize="12px" />
+                            <YAxis stroke="#adb5bd" dataKey="ping" fontSize="12px" />
+                            <Tooltip />
+                            {/* <Legend /> */}
+                            <Line type="monotone" dataKey="ping" stroke="#0088CC" strokeWidth="2px" />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
+            </Modal.Body>
+        </Modal>
             <h5 className="m-0">
                 <Link
                     to="#"
@@ -50,92 +154,19 @@ const PagesList = ({item, pagesData}) => {
             </h5>
             <Collapse in={open} appear>
                 <div>
-                    {pagesData.pages.map((page,index) => {return <p key={`${item}-${index}`} className="mt-2 mb-0"><a target="_blank" href={page.url}>{page.name}</a> / <a onClick={() => showChart(pagesData.service_name,page.name)}><i className='mdi mdi-chart-timeline-variant bg-white text-primary widget-icon'></i></a> / status - {page.response_status==200 ? <span className="text-success">{page.response_status}</span> : <span className="text-danger">{page.response_status}</span>} / ping - {page.response_time}ms</p>})}
+                    {pagesData.pages.map((page,index) => {
+                        return <p key={`${item}-${index}`} className="mt-2 mb-0">
+                            <a target="_blank" href={page.url}>{page.name}</a>
+                            /
+                            <a onClick={() => showChart(pagesData.service_name,page.name,'d',7)}>
+                                <i className='mdi mdi-chart-timeline-variant bg-white text-primary widget-icon'></i>
+                            </a> / status - {page.response_status==200 ? <span className="text-success">{page.response_status}</span> : <span className="text-danger">{page.response_status}</span>} / ping - {page.response_time}ms</p>})}
                 </div>
             </Collapse>
         </>
     );
 };
 
-
-const api = new APICore();
-const colors = ['#0088CC'];
-let apexBarChartOpts = {
-    grid: {
-        padding: {
-            left: 0,
-            right: 0,
-        },
-    },
-    noData: {
-      text: 'No Data'
-    },
-    chart: {
-        height: 550,
-        type: 'area',
-        parentHeightOffset: 0,
-        toolbar: {
-            show: true,
-        },
-    },
-    dataLabels: {
-        enabled: false,
-    },
-    stroke: {
-        curve: 'straight',
-        width: 2,
-    },
-    zoom: {
-        enabled: false,
-    },
-    legend: {
-        show: false,
-    },
-    colors: colors,
-    xaxis: {
-        type: 'datetime',
-        tooltip: {
-            enabled: false,
-        },
-        axisBorder: {
-            show: false,
-        },
-    },
-    yaxis: {
-        labels: {
-            formatter: function (val) {
-                return Math.floor(val);
-            },
-            offsetX: -15,
-        },
-    },
-    fill: {
-        type: 'gradient',
-        gradient: {
-            type: 'vertical',
-            shadeIntensity: 1,
-            inverseColors: false,
-            opacityFrom: 0.45,
-            opacityTo: 0.1,
-            stops: [45, 100],
-        },
-    },
-};
-function CustomDot(props){
-    const { cx, cy, fill } = props;
-
-    return <svg x={cx-3} y={cy-3} fill={fill} >
-        <circle cx="3" cy="3" r="3" ></circle>     
-    </svg>
-}
-
-function CustomActiveDot(props){
-    const { cx, cy, fill } = props;
-
-    return <svg x={cx-5} y={cy-5} fill={fill} stroke="white" >
-        <circle cx="5" cy="5" r="5" ></circle>     
-    </svg>
-}
 const ServerStatusList = ({socketState, serverStatusData}) => {
     const [chartView, setChartView] = useState(false);
     const [isActiveChart, setIsActiveChart] = useState();
@@ -148,10 +179,6 @@ const ServerStatusList = ({socketState, serverStatusData}) => {
         setChartView(!chartView);
         if (!chartView) updateChart(serverStatusData.service_name, 'd', 1)
     }
-
-    useEffect(() => {
-        //updateChart(serverStatusData.service_name, 'd', 1)
-    },[apexBarChartData])
 
     const updateChart = async (service_name, time_period, time_value) => {
         const url = `${process.env.REACT_APP_SERVER_URL}/api/v1/chart/service?service_name=${service_name}&time_period=${time_period}&time_value=${time_value}`;
@@ -175,7 +202,7 @@ const ServerStatusList = ({socketState, serverStatusData}) => {
                 if (time_period === 'm') timestamp = timestamp*100*60*60*24
                 if (time_period === 'y') timestamp = timestamp*100*60*60*24*30
                 let time = new Date(new Date().getTime() - timestamp);
-                chartData.push({'x': `${time.getFullYear()}-${time.getMonth()+1}-${time.getDate()} ${time.getHours()}:${time.getMinutes()<10 ? "0"+time.getMinutes():time.getMinutes()}`, 'ping' : Math.round(value)})
+                chartData.push(formatChartData(time, value))
             })
             let apexBarChartData = {
                 name: 'Response Time',
@@ -244,16 +271,9 @@ const ServerStatusList = ({socketState, serverStatusData}) => {
                                     </button>
                                 </li>
                             </ul>
-                            {/* <Chart
-                                options={apexBarChartOpts}
-                                series={apexBarChartData}
-                                type="area"
-                                className="apex-charts mt-1"
-                                height={350}
-                            /> */}
                             {apexBarChartData.data ?              
                             <ResponsiveContainer height={280}>
-                                    <LineChart
+                                <LineChart
                                     data={apexBarChartData.data}
                                     margin={{
                                         top: 50,
@@ -261,15 +281,15 @@ const ServerStatusList = ({socketState, serverStatusData}) => {
                                         left: -20,
                                         bottom: 5,
                                     }}
-                                    >
-                                        {/* <CartesianGrid /> */}
-                                        <XAxis stroke="#adb5bd" dataKey="x" fontSize="12px" />
-                                        <YAxis stroke="#adb5bd" dataKey="ping" fontSize="12px" />
-                                        <Tooltip />
-                                        {/* <Legend /> */}
-                                        <Line type="monotone" dataKey="ping" stroke="#0088CC" strokeWidth="2px" />
-                                    </LineChart>
-                                </ResponsiveContainer>
+                                >
+                                    {/* <CartesianGrid /> */}
+                                    <XAxis stroke="#adb5bd" dataKey="x" fontSize="12px" />
+                                    <YAxis stroke="#adb5bd" dataKey="ping" fontSize="12px" />
+                                    <Tooltip />
+                                    {/* <Legend /> */}
+                                    <Line type="monotone" dataKey="ping" stroke="#0088CC" strokeWidth="2px" />
+                                </LineChart>
+                            </ResponsiveContainer>
                             :
                             null
                             }
@@ -284,3 +304,7 @@ const ServerStatusList = ({socketState, serverStatusData}) => {
 };
 
 export default ServerStatusList;
+
+function formatChartData(time, value) {
+    return { 'x': `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()} ${time.getHours()}:${time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()}`, 'ping': Math.round(value) };
+}
